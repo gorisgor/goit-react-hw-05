aee92ff3ef98edc5964814221a61f9f5f7046041
Ihor Matviienko <igor.matvienko1989@gmail.com>
2024-07-10 06:22:47 +0300

    u

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,12 @@
+import css from "./Navigation.module.css";
+
+export default function Navigation() {
+  return (
+    <header>
+      <ul>
+        <li>Home</li>
+        <li>Movies</li>
+      </ul>
+    </header>
+  );
+}
